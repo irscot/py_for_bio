08d950509e573c696387a808dee7010474db4586
Isiah S <112279071+irscot@users.noreply.github.com>
2025-09-19 00:44:55 -0500

    testing for loop and indexing.

--- ch_6/several_species.py
+++ ch_6/several_species.py
@@ -4,8 +4,17 @@ Printing out the gene names for all genes belonging to
 """
 
 # reading the data from the csv file
-data = open("data.csv")
+# each line is a row while each field in a line is a column
 
-# writing for loop
-for gene in data:
-    if 
+# first index[0] = species name, second index[1] = sequence, third index[2] = gene name, fourth index[3] = bases in sequence
+data = open(r"C:\Users\irsco\repos\py_for_bio\ch_6\data.csv")
+
+# writing for loop to loop over the data and print out gene names
+# for each iteration of D. melanogaster or D. simulans
+for section in data:
+    # getting rid of new line (\n) showing up in output by stripping the right
+    # and using split method to seperate each part of the data by their commas
+    # which brings each line into a list
+    section = section.rstrip().split(",")
+    species_name = section[0]
+    print(species_name)
