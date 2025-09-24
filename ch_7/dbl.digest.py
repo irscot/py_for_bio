db35f7d388f2ebcf5f1e212863f7ad8cddd7258a
Isiah S <112279071+irscot@users.noreply.github.com>
2025-09-24 03:22:58 -0500

    creating double digest exercise python file with instructions

---
+++ ch_7/dbl.digest.py
@@ -0,0 +1,11 @@
+"""
+In the chapter_7 file inside the exercises download, 
+there's a file called dna.txt which contains a made-up DNA sequence. 
+
+Predict the fragment lengths that we will
+get if we digest the sequence with two made-up 
+restriction enzymes – AbcI, whose
+recognition site is ANT*AAT, and AbcII, whose recognition site 
+is GCRW*TG (asterisks indicate the position of the cut site)
+"""
+
