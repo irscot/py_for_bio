08bbaae3d500356be9d695bd75088f520e571730
Isiah S <112279071+irscot@users.noreply.github.com>
2025-09-27 21:46:14 -0500

    had to code out rename because it already ran. importing shutil to run shutil.copy.

--- ch_9/basic_file.py
+++ ch_9/basic_file.py
@@ -1,4 +1,6 @@
 import os
-os.rename(r"ch_9\old.txt", r"ch_9\new.txt")
+import shutil
+# os.rename(r"ch_9\old.txt", r"ch_9\new.txt")
 
 # using shutil module to copy files.
+shutil.copy(r"ch_9\new.txt", r"ch_9\copy.txt")
